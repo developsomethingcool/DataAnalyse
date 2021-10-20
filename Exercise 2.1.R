722de6ff4f9f5551f6223d379e42b027720eff44
developsomethingcool <pometunalexander@gmail.com>
2021-10-20 19:42:15 +0200

    Exercise 2.1 complete

---
+++ Exercise 2.1.R
@@ -0,0 +1,17 @@
+#Exercise 2.1
+print(5 * (659 - 34))
+
+#Exercise 2.2
+a <- 103
+b <- 14
+
+outcome <- round(a / b, digits = 3)
+
+#Exercise 2.3
+a <- 103 #assign a value 103
+b <- 14 #assign a value 14
+
+outcome <- round(a / b, digits = ) #divide a and b and round the answer to three digits
+
+#Res
+signif(x, digits = 2) # rounds the values in first argument to 2 significant digits
